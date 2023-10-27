7e5c39ecf2547db90d0819aca5ff11c9d3b7603e
Aghahowa Favour AIGBEKAEN <aigbekaenfavour@gmail.com>
2023-10-27 11:41:02 -0700

    edited Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,9 +7,7 @@ const Hero = () => {
     <>
         <div className='px-6 md:px-8 lg:px-24 grid justify-center md:flex md:justify-normal md:flex-row-reverse md:gap-9 text-center md:text-left'>
         <div className='flex justify-center'>
-            {/* <img src={banddoc} alt="Hero Image" className='w-[500px] md:w-[1300px] lg:w-[1500px]' /> */}
             <img src={hlf} alt="" className='w-[500px] md:w-[1300px] lg:w-[1500px]' />
-            {/* <img src={banddoc} alt="Hero Image" className='absolute h-[450px] w-[330px] top-14 sm:top-8 sm:w-[430px] sm:h-[570px] md:w-[400px] md:h-[560px]' /> */}
         </div>
         <div className='md:justify-items-start mt-3 md:mt-0 gap-2 hidden md:flex justify-center flex-col'>
             <p className='text-5xl md:mt-16 lg:mt-0 '>Your Child's Health,</p>
