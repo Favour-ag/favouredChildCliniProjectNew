cb20ccd8ea900e237926a7170e765a3948a4176f
Aghahowa Favour AIGBEKAEN <aigbekaenfavour@gmail.com>
2023-10-27 15:49:18 -0700

    fixed bug

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,27 +11,27 @@ const About = () => {
         <div className='grid justify-items-center md:flex md:px-6 lg:px-32 md:gap-10 lg:gap-6 mt-10 text-center'>
             <img src={mandw} alt="doctors"  className='w-[400px] h-[580px] md:h-[600px] rounded-xl'/>
                 <div className='grid justify-items-center p-6'>
-                <h1 className='text-xl font-semibold'>About Us</h1>
+                <h1 className='text-4xl font-semibold'>About Us</h1>
                 <p className='text-slate-500'>At FavouredChild Clinic, we're dedicated to one remarkable mission: Nurturing Every Child's Potential.</p>
                 <p className='mb-4 text-slate-500'>Our journey began with a simple, yet profound belief – that every child is a bundle of limitless potential, deserving the finest care. We've made it our life's work to transform this belief into a reality.</p>
                 <div className='grid gap-5 md:flex md:flex-wrap'>
                     <div className='w-[250px] h-[200px] shadow-lg shadow-slate-400 grid justify-items-center rounded-2xl p-4'>
-                      <FaHandsHoldingChild className='text-5xl' />
+                      <FaHandsHoldingChild  className='text-5xl text-green-400 ' />
                       <p className='text-lg font-semibold'>Child-Centered Excellence</p>
                       <p className=''>Our commitment revolves around the health, happiness, and wellbeing of children </p>
                     </div>
                     <div className='w-[250px] h-[200px] shadow-lg shadow-slate-400 grid justify-items-center rounded-2xl p-4'>
-                      <AiFillSafetyCertificate className='text-5xl' />
+                      <AiFillSafetyCertificate className='text-5xl text-green-400' />
                       <p className='text-lg font-semibold'>Safety First</p>
                       <p className=''>We follow stringent safety and hygiene protocols to ensure your child's health is never compromised </p>
                     </div>
                     <div className='w-[250px] h-[200px] shadow-lg shadow-slate-400 grid justify-items-center rounded-2xl p-4'>
-                      <TbShieldSearch className='text-5xl' />
+                      <TbShieldSearch className='text-5xl text-green-400' />
                       <p className='text-lg font-semibold'>Safety First</p>
                       <p className=''>We follow stringent safety and hygiene protocols to ensure your child's health is never compromised </p>
                     </div>
                     <div className='w-[250px] h-[200px] shadow-lg shadow-slate-400 grid justify-items-center rounded-2xl p-4'>
-                      <FaUserNurse className='text-5xl' />
+                      <FaUserNurse className='text-5xl text-green-400' />
                       <p className='text-lg font-semibold'>Experienced Pediatricians</p>
                       <p className=''>Our team of highly qualified and experienced pediatricians is dedicated to providing the best care for your child</p>
                     </div>
